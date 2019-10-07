048c0afe12ae3b562b8545d6e0d1250ab34cd203
jordan hagood <jordan.hagood@yahoo.com>
2019-10-07 16:43:54 -0700

    apiRoutes GET and POST routes for /api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,18 @@
-d
+var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+
+        var newFriend = req.body;
+        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        console.log(newFriend);
+
+        friends.push(newFriend);
+
+    });
+};
