257c87864bbd79c910cd9fd88e5bf127e543d2eb
jordan hagood <jordan.hagood@yahoo.com>
2019-10-07 16:29:24 -0700

    friends.js array of friend objects

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "VSCOGirlSupreme",
+        photo: "https://amp.businessinsider.com/images/5d7259312e22af1bf6247cf7-750-562.jpg",
+        scores: [5,5,5,5,5,1,5,5,3,1]
+    },
+    {
+        name: "Yessica",
+        photo: "https://pbs.twimg.com/profile_images/1080556534566871040/tzywcyx-_400x400.jpg",
+        scores: [3,2,4,5,3,1,2,5,3,1]
+    },
+    {
+        name: "Nicoya",
+        photo: "https://a.wattpad.com/cover/6461311-352-k757523.jpg",
+        scores: [1,1,1,4,4,1,1,1,3,3]
+    },
+    {
+        name: "Sharon",
+        photo: "https://cdn11.bigcommerce.com/s-whht7065/images/stencil/1280x1280/products/13676/17764/SF-SOCCERMOM-WHITEGRY__72762.1538071445.jpg?c=2&imbypass=on",
+        scores: [3,2,3,5,2,4,1,4,5,5]
+    },
+    {
+        name: "Brittany",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQy0Rv1FB7KoT2qhbUrG9cFoD6uLwSaDkO_hHTrOEE5Pnuf20HYQ",
+        scores: [1,2,2,1,1,5,4,2,4,4]
+    },
+    {
+        name: "Shadow",
+        photo: "https://s3.r29static.com/bin/entry/d7e/43,782,2428,2913/760x912,85/2174190/image.webp",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        name: "Brad",
+        photo: "https://pbs.twimg.com/media/CbwerIAUsAA8Rzq.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,5]
+    },
+    {
+        name: "Chad",
+        photo: "https://i.pinimg.com/originals/07/f7/c1/07f7c19a14c89cd0fc46656ca628c162.jpg",
+        scores: [5,3,4,2,3,1,4,5,2,4]
+    },
+    {
+        name: "Lex",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBF34h3klEOmXOkqYDVzvMadGrSGemY5l0OU_lx778QhZ6P8DO",
+        scores: [1,2,3,1,4,1,2,3,4,5]
+    },
+    {
+        name: "Dwell",
+        photo: "https://66.media.tumblr.com/ddbce388437b93bcca5f8914408a4bda/tumblr_nxae2x4HFR1shq991o1_400.jpg",
+        scores: [3,1,1,3,5,2,1,1,1,3]
+    }
+];
+
+module.exports = friends;
