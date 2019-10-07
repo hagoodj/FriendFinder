fa1578e6d8f23c79d5eec7340394a72e4dada10a
jordan hagood <jordan.hagood@yahoo.com>
2019-10-07 16:53:59 -0700

    calculates friend with minimum difference in scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,23 @@ module.exports = function (app) {
         newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
         console.log(newFriend);
 
+        var friendIndex;
+        var lowestDifference = 40;
+
+        for (var i = 0; i < friends.length; i++) {
+            var totalDifference = 0
+            for (var j = 0; j < 10; j++) {
+                var individualDifference = Math.abs(parseInt(newFriend.scores[j]) - friends[i].scores[j]);
+                totalDifference += individualDifference;
+            }
+            if (totalDifference < lowestDifference) {
+                lowestDifference = totalDifference;
+                friendIndex = i;
+            }
+        }
+
         friends.push(newFriend);
+        res.json(friends[friendIndex]);
 
     });
 };
